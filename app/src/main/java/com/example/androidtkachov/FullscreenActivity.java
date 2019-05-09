78d119d1796a010d96cd1375b0526ef24eb4ac08
maxumtkach <makstkach@bigmir.net>
2019-05-09 08:22:58 +0300

    //1.1.2   Hello world

---
+++ app/src/main/java/com/example/androidtkachov/FullscreenActivity.java
@@ -0,0 +1,26 @@
+package com.example.androidtkachov;
+
+import android.annotation.SuppressLint;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class FullscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen);
+
+    }
+}
